594f948d7c2f8f95ff7ed35b7c795157cc049798
Jordan Kaye <jordan.kaye2@gmail.com>
2025-06-02 06:10:10 -0600

    Bootstrap: trying another way

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,13 @@
-#!/bin/bash -l
+#!/bin/bash
 
 wget -O /tmp/nix-install https://nixos.org/nix/install
 chmod +x /tmp/nix-install
 /tmp/nix-install --daemon --yes --nix-extra-conf-file "$HOME/.dotfiles/nix.conf"
 sudo chown -R gitpod /nix
-nix-channel --add https://nixos.org/channels/nixos-25.05 nixpkgs
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz home-manager
-nix-channel --update
-nix-shell '<home-manager>' -A install
 ln -s "$HOME/.dotfiles" "$HOME/.config/home-manager"
-rm -rf ~/.config/home-manager/ ~/.bash_profile ~/.bashrc 
-home-manager switch
+bash -l -c "nix-channel --add https://nixos.org/channels/nixos-25.05 nixpkgs &&
+nix-channel --add https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz home-manager &&
+nix-channel --update &&
+nix-shell '<home-manager>' -A install &&
+rm -rf ~/.config/home-manager/ ~/.bash_profile ~/.bashrc  &&
+home-manager switch"
