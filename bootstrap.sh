5e2fdad6b875bf129b190882e7def9b63b6a7486
Jordan Kaye <jordan.kaye2@gmail.com>
2025-06-02 05:50:18 -0600

    Bootstrap: try login shell
    Just a test, not sure if this will work

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/bin/bash -l
 
 wget -O /tmp/nix-install https://nixos.org/nix/install
 chmod +x /tmp/nix-install
 /tmp/nix-install --daemon --yes --nix-extra-conf-file "$HOME/.dotfiles/nix.conf"
 sudo chown -R gitpod /nix
-bash -l -c "nix-channel --add https://nixos.org/channels/nixos-25.05 nixpkgs"
-bash -l -c "nix-channel --add https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz home-manager"
-bash -l -c "nix-channel --update"
-bash -l -c "nix-shell '<home-manager>' -A install"
-rm -rf ~/.config/home-manager/ ~/.bash_profile ~/.bashrc 
+nix-channel --add https://nixos.org/channels/nixos-25.05 nixpkgs
+nix-channel --add https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz home-manager
+nix-channel --update
+nix-shell '<home-manager>' -A install
 ln -s "$HOME/.dotfiles" "$HOME/.config/home-manager"
-# bash -l -c "home-manager switch"
+rm -rf ~/.config/home-manager/ ~/.bash_profile ~/.bashrc 
+home-manager switch
