604fd915d8e3e5a2a93fc8c962070732c2d26355
Jordan Kaye <jordan.kaye2@gmail.com>
2025-06-02 06:20:16 -0600

    Bootstrap: fix symlinking

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,10 +4,10 @@ wget -O /tmp/nix-install https://nixos.org/nix/install
 chmod +x /tmp/nix-install
 /tmp/nix-install --daemon --yes --nix-extra-conf-file "$HOME/.dotfiles/nix.conf"
 sudo chown -R gitpod /nix
-ln -s "$HOME/.dotfiles" "$HOME/.config/home-manager"
 bash -l -c "nix-channel --add https://nixos.org/channels/nixos-25.05 nixpkgs &&
 nix-channel --add https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz home-manager &&
 nix-channel --update &&
 nix-shell '<home-manager>' -A install &&
 rm -rf ~/.config/home-manager/ ~/.bash_profile ~/.bashrc  &&
+ln -s $HOME/.dotfiles $HOME/.config/home-manager &&
 home-manager switch"
