1c238e1cac60f95f4af4fb6d91750a2bd7c41f4a
Jordan Kaye <jordan.kaye2@gmail.com>
2026-02-06 13:36:20 -0700

    Moves dotfiles to private

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,12 +19,12 @@ done
 
 wget -O /tmp/nix-install https://nixos.org/nix/install
 chmod +x /tmp/nix-install
-/tmp/nix-install --daemon --yes --nix-extra-conf-file "$HOME/.dotfiles/nix.conf"
+/tmp/nix-install --daemon --yes --nix-extra-conf-file "$HOME/.private-dotfiles/nix.conf"
 sudo chown -R gitpod /nix
 bash -l -c "nix-channel --add https://nixos.org/channels/nixos-25.05 nixpkgs &&
 nix-channel --add https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz home-manager &&
 nix-channel --update &&
 nix-shell '<home-manager>' -A install &&
 rm -rf ~/.config/home-manager/ ~/.bash_profile ~/.bashrc  &&
-ln -s $HOME/.dotfiles $HOME/.config/home-manager &&
+ln -s $HOME/.private-dotfiles $HOME/.config/home-manager &&
 home-manager switch -b bak"
