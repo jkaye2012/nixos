4c22cbd0d488705ff34e4fe09d42f87081108203
Jordan Kaye <jordan.kaye2@gmail.com>
2026-04-09 14:33:41 -0600

    Upgrade to 25.11

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,8 +21,8 @@ wget -O /tmp/nix-install https://nixos.org/nix/install
 chmod +x /tmp/nix-install
 /tmp/nix-install --daemon --yes --nix-extra-conf-file "$HOME/.private-dotfiles/nix.conf"
 sudo chown -R gitpod /nix
-bash -l -c "nix-channel --add https://nixos.org/channels/nixos-25.05 nixpkgs &&
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz home-manager &&
+bash -l -c "nix-channel --add https://nixos.org/channels/nixos-25.11 nixpkgs &&
+nix-channel --add https://github.com/nix-community/home-manager/archive/release-25.11.tar.gz home-manager &&
 nix-channel --update &&
 nix-shell '<home-manager>' -A install &&
 rm -rf ~/.config/home-manager/ ~/.bash_profile ~/.bashrc  &&
